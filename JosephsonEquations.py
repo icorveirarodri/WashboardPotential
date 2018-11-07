8596d695d5793d7ab10b2b04a2e9f0afb9031cd4
mgely <mario.f.gely@gmail.com>
2018-11-07 15:57:02 +0100

    debugged, voltage phase evolution working

--- JosephsonEquations.py
+++ JosephsonEquations.py
@@ -1,30 +1,23 @@
-import stlab
 import numpy as np
-from scipy import integrate
+from scipy.integrate import quad
 from matplotlib import pyplot as plt, cm, colors
-from scipy import pi,hbar,e
+from scipy.constants import pi,hbar,e,h
+flux_0 = h/2./e
 
-def phase_evolution (U, time,N):
-	t_list = np.linspace(0,time,N)
-	phase_dif_list = []
-
-	U_list =  np.linspace(U, U, N) 
-
-	for i in range (0, N):
-		func = lambda x: (2*e*U*x/h_bar) 
-		phase_final = (integrate.quad(func,0,t_list[i])[0])
-		phase_dif_list.append(phase_final)
-
-	return(phase_dif_list,t_list)
+def phase_evolution (voltage_function,t_list):
+	return np.array([quad(voltage_function,0.,t)[0] for t in t_list])
 
 def potential (Ic, phase_list):
-	potential_list = (h_bar*Ic/e)*np.cos(phase_list)
-	return (potential_list)
+	return (hbar*Ic/e)*np.cos(2.*pi*phase_list)
 
 
+N = 1001
+def voltage(t):
+	return 3.*t
+t_list = np.linspace(0,1,N)
 
-phase_list, t_list = phase_evolution(2,10, 40)
-potential_list = potential (2, phase_list)
+phase_list =phase_evolution(voltage,t_list)
+potential_list = potential (2., phase_list)
 
 plt.plot(phase_list, potential_list)
 plt.xlabel('Phase')
