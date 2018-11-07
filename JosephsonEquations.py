a8567baa83e915d7de5f40dfb49b2837ff2270b6
mgely <mario.f.gely@gmail.com>
2018-11-07 16:34:13 +0100

    Correct equations now

--- JosephsonEquations.py
+++ JosephsonEquations.py
@@ -4,22 +4,39 @@ from matplotlib import pyplot as plt, cm, colors
 from scipy.constants import pi,hbar,e,h
 flux_0 = h/2./e
 
-def phase_evolution (voltage_function,t_list):
-	return np.array([quad(voltage_function,0.,t)[0] for t in t_list])
+def potential(t_list,Ic,voltage_function=None,current_function = None):
 
-def potential (Ic, phase_list):
-	return (hbar*Ic/e)*np.cos(2.*pi*phase_list)
+	if current_function == None:
+		# compute voltage evolution
+		phase_list = np.array([quad(voltage_function,0.,t)[0] for t in t_list])
+		current_list = Ic*np.sin(2.*pi*phase_list)
+		voltage_list = np.vectorize(voltage_function)(t_list)
+
+	if voltage_function == None:
+		# compute current evolution
+		current_list = np.vectorize(current_function)(t_list)
+		phase_list = np.arcsin(current_list/Ic)
+		voltage_list = np.concatenate([(phase_list[1:]-phase_list[:-1])/(t_list[1]-t_list[0]),[0]])
+
+	potential_list = np.array([np.trapz(voltage_list[:i]*current_list[:i],t_list[:i]) for i in range(len(t_list))])
+	return phase_list,potential_list,current_list,voltage_list
 
 
 N = 1001
 def voltage(t):
-	return 3.*t
-t_list = np.linspace(0,1,N)
+	return 1.
+def current(t):
+	return 1.
+t_list = np.linspace(0,100.,N)
+Ic = 1.
 
-phase_list =phase_evolution(voltage,t_list)
-potential_list = potential (2., phase_list)
+# phase,U,I,V = potential(t_list,Ic,voltage_function=voltage,current_function = None)
+phase,U,I,V = potential(t_list,Ic,voltage_function=None,current_function = current)
 
-plt.plot(phase_list, potential_list)
+plt.plot(I)
+plt.plot(V)
+
+# plt.plot(phase,U)
 plt.xlabel('Phase')
 plt.ylabel('Potential')
 plt.show()
