c0ee00a910dc54c8169e3901b328674792b7af42
icorveirarodri <I.C.CorveiraRodrigues@tudelft.nl>
2018-11-07 15:29:19 +0100

    stuff

---
+++ JosephsonEquations.py
@@ -0,0 +1,37 @@
+import stlab
+import numpy as np
+from scipy import integrate
+from matplotlib import pyplot as plt, cm, colors
+pi = np.pi
+
+#Variables 
+h_bar = 1.0545e-34 #J.s
+e = 1.6e-19
+
+def phase_evolution (U, time,N):
+	t_list = np.linspace(0,time,N)
+	phase_dif_list = []
+
+	U_list =  np.linspace(U, U, N) 
+
+	for i in range (0, N):
+		func = lambda x: (2*e*U*x/h_bar) 
+		phase_final = (integrate.quad(func,0,t_list[i])[0])
+		phase_dif_list.append(phase_final)
+
+	return(phase_dif_list,t_list)
+
+def potential (Ic, phase_list):
+	potential_list = (h_bar*Ic/e)*np.cos(phase_list)
+	return (potential_list)
+
+
+
+phase_list, t_list = phase_evolution(2,10, 40)
+potential_list = potential (2, phase_list)
+
+plt.plot(phase_list, potential_list)
+plt.xlabel('Phase')
+plt.ylabel('Potential')
+plt.show()
+plt.close()
